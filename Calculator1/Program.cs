2f5210ab1107aa1430ec84f0ce38e3e164cd4150
Davon Daniels <38470284+felician23@users.noreply.github.com>
2025-02-12 21:26:01 -0600

    C# Calculator finished

---
+++ Calculator1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int result;
+
+            string answer;
+            string close;
+
+            Console.WriteLine("Welcome to Calculator Program");
+            Console.WriteLine("Please enter your first number");
+
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter your second number");
+
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What type of operation would you like to do?");
+            Console.WriteLine("Please enter a for addition, s for subtraction, m for multiplication, r for remainder or any other key for division");
+
+            answer = Console.ReadLine();
+
+            if (answer == "a")
+            {
+                result = num1 + num2;
+            }
+            else if (answer == "s")
+            {
+                result = num1 - num2;
+            }
+            else if (answer == "m")
+            {
+                result = num1 * num2;
+            }
+            else if (answer == "r")
+            {
+                result = num1 % num2;
+            }
+            else
+            {
+                result = num1 / num2;
+            }
+
+            Console.WriteLine("The result is " + result);
+            Console.WriteLine("Thank you for using our app. press c to close");
+
+            close = Console.ReadLine();
+
+            if (close == "c")
+            {
+                Environment.Exit(0);
+            }
+
+            Console.ReadKey(); 
+        }
+    }
+}
